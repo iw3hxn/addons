95d7e0269e166a98e9f3f2f5ea224a3c7268e104
Carlo <carlo.vettore@didotech.com>
2015-07-27 10:00:59 +0200

    [fix]

--- sale_crm/sale_crm.py
+++ sale_crm/sale_crm.py
@@ -21,13 +21,47 @@
 
 from osv import osv,fields
 
+
 class sale_order(osv.osv):
     _inherit = 'sale.order'
+
+    def onchange_partner_id(self, cr, uid, ids, part):
+        res = super(sale_order, self).onchange_partner_id(cr, uid, ids, part)
+        section = self.pool.get('res.partner').browse(cr, uid, part).section_id
+        if section:
+            res['value']['section_id'] = section.id
+        return res
+
+    def _get_default_section_id(self, cr, uid, context=None):
+        """ Gives default section by checking if present in the context """
+        section_id = self._resolve_section_id_from_context(cr, uid, context=context) or False
+        if not section_id:
+            section_id = self.pool.get('res.users').browse(cr, uid, uid, context).default_section_id.id or False
+        return section_id
+
+    def _resolve_section_id_from_context(self, cr, uid, context=None):
+        """ Returns ID of section based on the value of 'section_id'
+            context key, or None if it cannot be resolved to a single
+            Sales Team.
+        """
+        if context is None:
+            context = {}
+        if type(context.get('default_section_id')) in (int, long):
+            return context.get('default_section_id')
+        if isinstance(context.get('default_section_id'), basestring):
+            section_ids = self.pool.get('crm.case.section').name_search(cr, uid, name=context['default_section_id'], context=context)
+            if len(section_ids) == 1:
+                return int(section_ids[0][0])
+        return None
 
     _columns = {
         'section_id': fields.many2one('crm.case.section', 'Sales Team'),
         'categ_id': fields.many2one('crm.case.categ', 'Category', \
             domain="['|',('section_id','=',section_id),('section_id','=',False), ('object_id.model', '=', 'crm.lead')]")
+    }
+ 
+    _default = {
+        'section_id': lambda s, cr, uid, c: s._get_default_section_id(cr, uid, c),
     }
 
 sale_order()
