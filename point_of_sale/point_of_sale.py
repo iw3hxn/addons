2f99c3e755869bf75659c4f9ff8733b3239a6cb7
carlo <carlo.vettore@didotech.com>
2018-07-18 16:47:34 +0200

    mini fix for get correct journal and also account based

--- point_of_sale/point_of_sale.py
+++ point_of_sale/point_of_sale.py
@@ -356,8 +356,7 @@ class pos_order(osv.osv):
         if data.get('payment_name', False):
             args['name'] = args['name'] + ': ' + data['payment_name']
         account_def = property_obj.get(cr, uid, 'property_account_receivable', 'res.partner', context=context)
-        args['account_id'] = (order.partner_id and order.partner_id.property_account_receivable \
-                             and order.partner_id.property_account_receivable.id) or order.shop_id and order.shop_id.property_account_receivable and order.shop_id.property_account_receivable.id or (account_def and account_def.id) or False
+        args['account_id'] = (order.partner_id and order.partner_id.property_account_receivable and order.partner_id.property_account_receivable.id) or (order.shop_id and order.shop_id.property_account_receivable and order.shop_id.property_account_receivable.id) or (account_def and account_def.id) or False
         args['partner_id'] = order.partner_id and order.partner_id.id or None
 
         if not args['account_id']:
