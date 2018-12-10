b13e8f8d7f6e112219eef2efb729569925868034
Antonio Mignolli <antonio.mignolli@didotech.com>
2018-12-10 16:48:18 +0100

    [account_bank_statement_import] moved

--- account_bank_statement_import/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# © 2013-2018 Antonio Mignolli - Didotech srl (www.didotech.com)
-
-from . import models
-from . import wizards
--- account_bank_statement_import/wizards/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-# © 2013-2018 Antonio Mignolli - Didotech srl (www.didotech.com)
-
-from . import account_bank_statement_import
