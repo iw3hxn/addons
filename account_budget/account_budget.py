a8f8025913e8afae2a1ae0a3a68b6072ae45ca5e
carlo <carlo.vettore@didotech.com>
2019-12-13 15:14:53 +0100

    [account_budget] fix calculation of budget

--- account_budget/account_budget.py
+++ account_budget/account_budget.py
@@ -18,13 +18,13 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-
 import datetime
-
-from osv import osv, fields
-from tools.translate import _
+from operator import add
 
 import decimal_precision as dp
+from openerp.osv import fields, osv
+from openerp.tools.translate import _
+
 
 def strToDate(dt):
         dt_date=datetime.date(int(dt[0:4]),int(dt[5:7]),int(dt[8:10]))
@@ -62,7 +62,7 @@ class crossovered_budget(osv.osv):
         'validating_user_id': fields.many2one('res.users', 'Validate User', readonly=True),
         'date_from': fields.date('Start Date', required=True, states={'done':[('readonly',True)]}),
         'date_to': fields.date('End Date', required=True, states={'done':[('readonly',True)]}),
-        'state' : fields.selection([('draft','Draft'),('confirm','Confirmed'),('validate','Validated'),('done','Done'),('cancel', 'Cancelled')], 'Status', select=True, required=True, readonly=True),
+        'state' : fields.selection([('draft','Draft'),('cancel', 'Cancelled'),('confirm','Confirmed'),('validate','Validated'),('done','Done')], 'Status', select=True, required=True, readonly=True),
         'crossovered_budget_line': fields.one2many('crossovered.budget.lines', 'crossovered_budget_id', 'Budget Lines', states={'done':[('readonly',True)]}),
         'company_id': fields.many2one('res.company', 'Company', required=True),
     }
@@ -104,9 +104,179 @@ class crossovered_budget(osv.osv):
         })
         return True
 
-crossovered_budget()
 
 class crossovered_budget_lines(osv.osv):
+
+    def _get_period_range_from_start_period(self, cr, uid, start_period, include_opening=False,
+                                            fiscalyear=False, stop_at_previous_opening=False):
+        """We retrieve all periods before start period"""
+        opening_period_id = False
+        past_limit = []
+        period_obj = self.pool.get('account.period')
+        mv_line_obj = self.pool.get('account.move.line')
+        # We look for previous opening period
+        date_start = date_stop = start_period
+
+        if stop_at_previous_opening:
+            opening_search = [('special', '=', True),
+                              ('date_stop', '<', date_start)]
+            if fiscalyear:
+                opening_search.append(('fiscalyear_id', '=', fiscalyear.id))
+
+            opening_periods = period_obj.search(cr, uid, opening_search,
+                                                order='date_stop desc')
+            for opening_period in opening_periods:
+                validation_res = mv_line_obj.search(cr, uid,
+                                                    [('period_id', '=', opening_period)],
+                                                    limit=1)
+                if validation_res:
+                    opening_period_id = opening_period
+                    break
+            if opening_period_id:
+                # we also look for overlapping periods
+                opening_period_br = period_obj.browse(cr, uid, opening_period_id)
+                past_limit = [('date_start', '>=', opening_period_br.date_stop)]
+
+        periods_search = [('date_stop', '<=', date_stop)]
+        periods_search += past_limit
+
+        if not include_opening:
+            periods_search += [('special', '=', False)]
+
+        if fiscalyear:
+            periods_search.append(('fiscalyear_id', '=', fiscalyear.id))
+        periods = period_obj.search(cr, uid, periods_search)
+        if include_opening and opening_period_id:
+            periods.append(opening_period_id)
+        periods = list(set(periods))
+
+        return periods
+
+    def _compute_init_balance(self, account_id=None, period_ids=None, mode='computed', default_values=False):
+        if not isinstance(period_ids, list):
+            period_ids = [period_ids]
+        res = {}
+
+        if not default_values:
+            if not account_id or not period_ids:
+                raise Exception('Missing account or period_ids')
+            try:
+                self.cursor.execute("SELECT sum(debit) AS debit, "
+                                    " sum(credit) AS credit, "
+                                    " sum(debit)-sum(credit) AS balance, "
+                                    " sum(amount_currency) AS curr_balance"
+                                    " FROM account_move_line"
+                                    " WHERE period_id in %s"
+                                    " AND account_id = %s", (tuple(period_ids), account_id))
+                res = self.cursor.dictfetchone()
+
+            except Exception, exc:
+                self.cursor.rollback()
+                raise
+
+        return {'debit': res.get('debit') or 0.0,
+                'credit': res.get('credit') or 0.0,
+                'init_balance': res.get('balance') or 0.0,
+                'init_balance_currency': res.get('curr_balance') or 0.0,
+                'state': mode}
+
+    def get_included_opening_period(self, cr, uid, period):
+        """Return the opening included in normal period we use the assumption
+        that there is only one opening period per fiscal year"""
+        period_obj = self.pool['account.period']
+        date_start = date_stop = period
+        return period_obj.search(cr, uid,
+                                 [('special', '=', True),
+                                  ('date_start', '>=', date_start),
+                                  ('date_stop', '<=', date_stop)],
+                                  limit=1)
+
+    def _compute_initial_balances(self, cr, uid, account_ids, start_period, fiscalyear):
+        """We compute initial balance.
+        If form is filtered by date all initial balance are equal to 0
+        This function will sum pear and apple in currency amount if account as no secondary currency"""
+        # if opening period is included in start period we do not need to compute init balance
+        # we just read it from opening entries
+        res = {}
+        # PNL and Balance accounts are not computed the same way look for attached doc
+        # We include opening period in pnl account in order to see if opening entries
+        # were created by error on this account
+        pnl_periods_ids = self._get_period_range_from_start_period(cr, uid, start_period, fiscalyear=fiscalyear,
+                                                                   include_opening=True)
+        bs_period_ids = self._get_period_range_from_start_period(cr, uid, start_period, include_opening=True,
+                                                                 stop_at_previous_opening=True)
+        opening_period_selected = self.get_included_opening_period(cr, uid, start_period)
+
+        for acc in self.pool.get('account.account').browse(cr, uid, account_ids):
+            res[acc.id] = self._compute_init_balance(default_values=True)
+            if acc.user_type.close_method == 'none':
+                # we compute the initial balance for close_method == none only when we print a GL
+                # during the year, when the opening period is not included in the period selection!
+                if pnl_periods_ids and not opening_period_selected:
+                    res[acc.id] = self._compute_init_balance(acc.id, pnl_periods_ids)
+            else:
+                res[acc.id] = self._compute_init_balance(acc.id, bs_period_ids)
+        return res
+
+    def _get_account_details(self, cr, uid, account_ids, target_move, fiscalyear, main_filter, start, stop, initial_balance_mode, context=None):
+        """
+        Get details of accounts to display on the report
+        @param account_ids: ids of accounts to get details
+        @param target_move: selection filter for moves (all or posted)
+        @param fiscalyear: browse of the fiscalyear
+        @param main_filter: selection filter period / date or none
+        @param start: start date or start period browse instance
+        @param stop: stop date or stop period browse instance
+        @param initial_balance_mode: False: no calculation, 'opening_balance': from the opening period, 'initial_balance': computed from previous year / periods
+        @return: dict of list containing accounts details, keys are the account ids
+        """
+        if context is None:
+            context = {}
+
+        account_obj = self.pool.get('account.account')
+        period_obj = self.pool.get('account.period')
+        use_period_ids = main_filter in ('filter_no', 'filter_period', 'filter_opening')
+
+        if use_period_ids:
+            if main_filter == 'filter_opening':
+                period_ids = [start.id]
+            else:
+                period_ids = period_obj.build_ctx_periods(cr, uid, start.id, stop.id)
+                # never include the opening in the debit / credit amounts
+                period_ids = self.exclude_opening_periods(period_ids)
+
+        init_balance = False
+        if initial_balance_mode == 'opening_balance':
+            init_balance = self._read_opening_balance(account_ids, start)
+        elif initial_balance_mode:
+            init_balance = self._compute_initial_balances(cr, uid, account_ids, start, fiscalyear)
+
+        ctx = context.copy()
+        ctx.update({'state': target_move,
+                    'all_fiscalyear': True})
+
+        if use_period_ids:
+            ctx.update({'periods': period_ids,})
+        elif main_filter == 'filter_date':
+            ctx.update({'date_from': start,
+                        'date_to': stop})
+
+        accounts = account_obj.read(cr, uid, account_ids, ['type','code','name','debit','credit', 'balance', 'parent_id','level','child_id'], ctx)
+
+        accounts_by_id = {}
+        for account in accounts:
+            if init_balance:
+                # sum for top level views accounts
+                child_ids = account_obj._get_children_and_consol(cr, uid, account['id'], ctx)
+                if child_ids:
+                    child_init_balances = [init_bal['init_balance'] for acnt_id, init_bal in init_balance.iteritems() if acnt_id in child_ids ]
+                    top_init_balance = reduce(add, child_init_balances)
+                    account['init_balance'] = top_init_balance
+                else:
+                    account.update(init_balance[account['id']])
+                account['balance'] = account['init_balance'] + account['debit'] - account['credit']
+            accounts_by_id[account['id']] = account
+        return accounts_by_id
 
     def _prac_amt(self, cr, uid, ids, context=None):
         res = {}
@@ -118,13 +288,10 @@ class crossovered_budget_lines(osv.osv):
             acc_ids = [x.id for x in line.general_budget_id.account_ids]
             acc_ids = account_obj._get_children_and_consol(cr, uid, acc_ids, context=context)
             if not acc_ids:
-                raise osv.except_osv(_('Error!'),_("The Budget '%s' has no accounts!") % str(line.general_budget_id.name))
-            date_to = line.date_to
-            date_from = line.date_from
-            if context.has_key('wizard_date_from'):
-                date_from = context['wizard_date_from']
-            if context.has_key('wizard_date_to'):
-                date_to = context['wizard_date_to']
+                raise osv.except_osv(_('Error!'), _("The Budget '%s' has no accounts!") % str(line.general_budget_id.name))
+
+            date_to = context.get('wizard_date_to', line.date_to)
+            date_from = context.get('wizard_date_from', line.date_from)
             if line.analytic_account_id.id:
                 cr.execute("SELECT SUM(amount) FROM account_analytic_line WHERE account_id in "
                        """(with recursive account_analytic_account_hierarchy(id)
@@ -144,13 +311,23 @@ class crossovered_budget_lines(osv.osv):
                        "between to_date(%s,'yyyy-mm-dd') AND to_date(%s,'yyyy-mm-dd')) AND "
                        "general_account_id=ANY(%s)", (line.analytic_account_id.id, date_from, date_to,acc_ids,))
                 result = cr.fetchone()[0]
+            else:
+                fiscalyear_obj = self.pool['account.fiscalyear']
+                fiscalyear_id = fiscalyear_obj.find(cr, uid, dt=date_from, context=context)
+                fiscalyear = fiscalyear_obj.browse(cr, uid, fiscalyear_id, context)
+                accounts_by_ids = self._get_account_details(cr, uid, acc_ids, target_move='posted', fiscalyear=fiscalyear, main_filter='filter_date', start=date_from,
+                                                            stop=date_to, initial_balance_mode='initial_balance', context=context)
+                result = 0.0
+                for key in accounts_by_ids.keys():
+                    result += accounts_by_ids[key]['balance']
+
             if result is None:
                 result = 0.00
             res[line.id] = result
         return res
 
     def _prac(self, cr, uid, ids, name, args, context=None):
-        res={}
+        res = {}
         for line in self.browse(cr, uid, ids, context=context):
             res[line.id] = self._prac_amt(cr, uid, [line.id], context=context)[line.id]
         return res
@@ -161,12 +338,8 @@ class crossovered_budget_lines(osv.osv):
             context = {}
         for line in self.browse(cr, uid, ids, context=context):
             today = datetime.datetime.today()
-            date_to = today.strftime("%Y-%m-%d")
-            date_from = line.date_from
-            if context.has_key('wizard_date_from'):
-                date_from = context['wizard_date_from']
-            if context.has_key('wizard_date_to'):
-                date_to = context['wizard_date_to']
+            date_to = context.get('wizard_date_to', today.strftime("%Y-%m-%d"))
+            date_from = context.get('wizard_date_from', line.date_from)
 
             if line.paid_date:
                 if strToDate(line.date_to) <= strToDate(line.paid_date):
@@ -180,7 +353,7 @@ class crossovered_budget_lines(osv.osv):
                     elapsed = strToDate(date_to) - strToDate(date_to)
 
                 if total.days:
-                    theo_amt = float(elapsed.days / float(total.days)) * line.planned_amount
+                    theo_amt = float((elapsed.days + 1) / float(total.days + 1)) * line.planned_amount
                 else:
                     theo_amt = line.planned_amount
 
@@ -206,14 +379,14 @@ class crossovered_budget_lines(osv.osv):
     _description = "Budget Line"
     _columns = {
         'crossovered_budget_id': fields.many2one('crossovered.budget', 'Budget', ondelete='cascade', select=True, required=True),
-        'analytic_account_id': fields.many2one('account.analytic.account', 'Analytic Account',required=True),
+        'analytic_account_id': fields.many2one('account.analytic.account', 'Analytic Account'),
         'general_budget_id': fields.many2one('account.budget.post', 'Budgetary Position',required=True),
         'date_from': fields.date('Start Date', required=True),
         'date_to': fields.date('End Date', required=True),
         'paid_date': fields.date('Paid Date'),
-        'planned_amount':fields.float('Planned Amount', required=True, digits_compute=dp.get_precision('Account')),
-        'practical_amount':fields.function(_prac, string='Practical Amount', type='float', digits_compute=dp.get_precision('Account')),
-        'theoritical_amount':fields.function(_theo, string='Theoretical Amount', type='float', digits_compute=dp.get_precision('Account')),
+        'planned_amount': fields.float('Planned Amount', required=True, digits_compute=dp.get_precision('Account')),
+        'practical_amount': fields.function(_prac, string='Practical Amount', type='float', digits_compute=dp.get_precision('Account')),
+        'theoritical_amount': fields.function(_theo, string='Theoretical Amount', type='float', digits_compute=dp.get_precision('Account')),
         'percentage':fields.function(_perc, string='Percentage', type='float'),
         'company_id': fields.related('crossovered_budget_id', 'company_id', type='many2one', relation='res.company', string='Company', store=True, readonly=True)
     }
